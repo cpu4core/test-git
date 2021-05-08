50a4a5f64546939705a79049870974cad12994b2
CPU4Core <cpu4core@gmail.com>
2021-05-08 23:44:17 +0700

    commmit again on dev

--- test-git/Form1.vb
+++ test-git/Form1.vb
@@ -3,5 +3,6 @@
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'Line 1
         'Line 2
+        ' Line 3
     End Sub
 End Class
