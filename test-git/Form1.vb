1feb30972fe1940f39bfa00becb524ae3c9cae15
CPU4Core <cpu4core@gmail.com>
2021-05-08 22:43:48 +0700

    add line 2

--- test-git/Form1.vb
+++ test-git/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'Line 1
+        'write line 1
+        'write line 2
     End Sub
 End Class
