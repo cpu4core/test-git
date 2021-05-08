d58db8a0fa1eed9e7dff736cb243c5ee9c33f828
CPU4Core <cpu4core@gmail.com>
2021-05-08 23:47:20 +0700

    commit on feature2

--- test-git/Form1.vb
+++ test-git/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'write line 1
-        'write line 2
+        'write line 6
+        'write line 7
     End Sub
 End Class
